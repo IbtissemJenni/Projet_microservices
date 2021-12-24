b81d89af9a4425702a709e2cb72fe5cb2503aa3f
IbtissemJenni <ibtissem.jenni@esprit.tn>
2021-12-24 19:17:20 +0100

    first commit

---
+++ Micro-eureka-server/src/test/java/Eureka/TfgEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package Eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Micro-series-service/src/test/java/series/TfgSeriesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package series;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgSeriesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
